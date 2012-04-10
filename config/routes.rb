e6149a1fed7e94c7e9dc76e10a4c103a306602b5
Kush Patel <kushpatel72@gmail.com>
2012-04-10 16:07:23 -0700

    Adds signin/up links to home page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Pair::Application.routes.draw do
   devise_for :users
-
+  
+  devise_scope :user do
+    match "/signup" => "devise/registrations#new"
+    match "/signin" => "devise/sessions#new"
+  end
+  
   root :to => 'static_pages#home'
 end
