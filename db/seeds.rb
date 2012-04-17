1c7c2bed78e63136529037baf547f899385e0bfc
Kush Patel <kushpatel72@gmail.com>
2012-04-17 14:12:22 -0700

    Seeded languages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require_relative 'languages.rb'
+
+@langs.each do |element|
+  Language.create(:name => element)
+end
